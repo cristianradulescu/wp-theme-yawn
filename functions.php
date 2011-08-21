7f9d20c17d35fc2b5ef17f99118823403f759d84
Cristian Radulescu <cristian67@gmail.com>
2011-08-21 03:16:12 +0300

    Added admin menu; added google analytics support

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@
 /**
  * Register widgetized areas
  *
- * @uses register_sidebar
+ * @return void
  */
 function yawn_widgets_init() {
 	// Header, located at the top of the page.
@@ -33,6 +33,45 @@ function yawn_widgets_init() {
 		'after_title' => '</span>',
 	) );
 }
-/** Register sidebars by running yawn_widgets_init() on the widgets_init hook. */
-add_action( 'widgets_init', 'yawn_widgets_init' );
+add_action('widgets_init', 'yawn_widgets_init');
 
+/**
+ * Yawn administration menu item
+ * 
+ * @return void
+ */
+function yawn_admin_menu() {
+	add_theme_page('Manage Yawn theme options', 'Yawn options', 'manage_options', 'yawn-admin', 'yawn_admin');
+}
+add_action('admin_menu', 'yawn_admin_menu');
+
+/**
+ * Include the administration page
+ * 
+ * @return void
+ */
+function yawn_admin() {
+  require_once 'yawn-admin.php';
+}
+
+/**
+ * Add Google Analytics javascript is the code was set
+ * 
+ * @return void
+ */
+function yawn_google_analytics_wp_head()
+{
+  if (false !== get_option('yawn-google-analitycs-code')): ?>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php echo get_option('yawn-google-analitycs-code') ?>']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+  <?php endif; 
+}
+add_action('wp_head', 'yawn_google_analytics_wp_head');
---
+++ yawn-admin.php
@@ -0,0 +1,44 @@
+<?php
+
+$title = __('Yawn Dashboard');
+$parent_file = 'themes.php';
+
+// handle form submit
+if (isset($_POST['submit'])) {
+  // google analytics
+  update_option('yawn-google-analitycs-code', $_POST['yawn-google-analitycs-code']);
+}
+
+?>
+
+<div class="wrap">
+  <?php screen_icon(); ?>
+  <h2><?php echo esc_html( $title ); ?></h2>
+  <?php if (isset($_POST['submit'])): ?>
+  <div id="setting-error-settings_updated" class="updated settings-error"> 
+    <p><strong><?php echo _e('Settings saved.') ?></strong></p>
+  </div>
+  <?php endif; ?>
+
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>?page=yawn-admin">
+  
+    <table class="form-table">
+      <tr valign="top">
+        <th scope="col" colspan="2"><h3><?php _e('Google') ?></h3></th>
+      </tr>
+      <tr valign="top">
+        <th scope="row"><label for="yawn-google-analitycs-code"><?php _e('Analytics code') ?></label></th>
+        <td>
+          <input name="yawn-google-analitycs-code" 
+                 type="text" 
+                 id="yawn-google-analitycs-code" 
+                 value="<?php echo get_option('yawn-google-analitycs-code') ?>" 
+                 class="regular-text" />
+        </td>
+      </tr>
+    </table>
+    <?php submit_button() ?>
+    
+  </form>
+
+</div><!-- wrap -->
